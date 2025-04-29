046362ff8c3fc14c5f2e8966d3bb1ed4da939c32
stewart <202038944+Stewart-369@users.noreply.github.com>
2025-04-29 13:48:56 +0100

    Add getComputerChoice function that randomly returns rock, paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("hello, World!")
+function getcomputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+
+    switch (randomNum) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+console.log(getcomputerChoice());
