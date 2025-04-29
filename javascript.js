ec9d3ec73dddfa363633fe19f609a4fa675f37d4
stewart <202038944+Stewart-369@users.noreply.github.com>
2025-04-29 22:33:57 +0100

    Add the logic to play the entire game inside the playGame function

--- javascript.js
+++ javascript.js
@@ -15,21 +15,38 @@ function getHumanChoice() {
     return prompt("Please choose rock, paper or scissors").toLowerCase();
 }
 
+function playGame() {
+
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
+  if (humanChoice === computerChoice) {
         console.log("It's a tie!");
-    } else if (
+  } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
+  ) {
         console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
         humanScore++;
-    } else {
+  } else {
         console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
         computerScore++;
-    }
+  }
+}
+
+  for (let i = 1; i <= 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getcomputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You win the game!");
+  } else if (computerScore > humanScore) {
+    console.log("You lose the game!");
+  } else {
+    console.log("Its a tie!");
+  }
 }
