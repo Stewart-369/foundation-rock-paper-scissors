7a911a29f4f94ee5521f21d62ec7c2c435975ccb
stewart <202038944+Stewart-369@users.noreply.github.com>
2025-04-29 21:53:22 +0100

    Add playRound function and logic to compare choices and update scores

--- javascript.js
+++ javascript.js
@@ -12,7 +12,24 @@ function getcomputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Please choose rock, paper or scissors");
+    return prompt("Please choose rock, paper or scissors").toLowerCase();
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
